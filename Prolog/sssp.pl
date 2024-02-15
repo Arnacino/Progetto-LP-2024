39ca44eb987d44f286c76877556a13f4e51cd5a2
arancinosbarazzino <samueleferrillo16@gmail.com>
2024-02-15 12:44:58 +0100

    removed all of the -> and ; because fuck you...

--- Prolog/sssp.pl
+++ Prolog/sssp.pl
@@ -49,7 +49,6 @@ neighbors(G, V, Ns) :-
 
 list_edges(G) :- listing(edge(G, _, _, _)).
 
-%Predicato che stampa il grafo G
 list_graph(G) :-
     writeln("Vertici:"),
     list_vertices(G),
@@ -65,15 +64,22 @@ change_distance(G, V, NewD) :-
     retractall(distance(G, V, _)), 
     assert(distance(G, V, NewD)).
 
-
 change_previous(G, V, NewU) :- 
     retractall(previous(G, V, _)), 
     assert(previous(G, V, NewU)).
 
+visited(G, V) :- clause(visited(G, V), true), !.
+visited(G, V) :- assert(visited(G, V)).
+
 %Predicato che inizializza le distanze da tutti i vertici a infinito
 initialize_distances(_, _, []). % Caso base: la lista è vuota
+initialize_distances(G, Source, [Source | Rest]) :-
+    assert(distance(G, Source, 0)),
+    initialize_distances(G, Source, Rest).
+
 initialize_distances(G, Source, [V | Rest]) :-
-    (V = Source -> assert(distance(G, V, 0)); assert(distance(G, V, inf))),
+    V \= Source,
+    assert(distance(G, V, inf)),
     initialize_distances(G, Source, Rest).
 
 initialize_heap(_, []).
@@ -105,7 +111,20 @@ dijkstra_sssp(G, Source) :-
 dijkstra(G, _) :-
     empty(G), !.
 
-dijkstra(G, Natt) :- 
+dijkstra(G, Natt) :-
+    not_empty(G),
+    previous(G, Natt, Nprec),
+    extract(G, _, Natt),
+    visited(G, Natt),
+    assert(previous(G, Natt, Nprec)),
+    neighbors(G, Natt, Ns),
+    extractV_list(Ns, NsList),
+    process_neighbors(G, Natt, NsList),
+    not_empty(G),
+    head(G, _, Nsucc),
+    dijkstra(G, Nsucc), !.
+
+dijkstra(G, Natt) :-
     not_empty(G),
     previous(G, Natt, Nprec),
     extract(G, _, Natt),
@@ -114,26 +133,37 @@ dijkstra(G, Natt) :-
     neighbors(G, Natt, Ns),
     extractV_list(Ns, NsList),
     process_neighbors(G, Natt, NsList),
-    (not_empty(G) ->
-    head(G, _, Nsucc), dijkstra(G, Nsucc); true
-    ).
+    \+ not_empty(G).
 
 % Se un nodo è visitato allora non è nella heap e quindi non è possibile modificarne la chiave 
 % quindi la sua distanza si prende da distance e non dalla heap.
 process_neighbors(_, _, []) :- !.
 % Predicato che processa i vicini di un vertice   
 process_neighbors(G, Natt, [ V | Rest]) :-
-    visited(G, V) -> distance(G, V, K), distance_calc(G, Natt, V, K, Rest); 
-    heap_entry(G, _, K, V), distance_calc(G, Natt, V, K, Rest).
+    visited(G, V),
+    distance(G, V, K),
+    distance_calc(G, Natt, V, K, Rest), !.
+
+process_neighbors(G, Natt, [ V | Rest]) :-
+    \+ visited(G, V),
+    heap_entry(G, _, K, V),
+    distance_calc(G, Natt, V, K, Rest).
 
 distance_calc(G, Natt, V, K, Rest):-
     edge(G, Natt, V, W),
     distance(G, Natt, Datt),
     NewDistance is W + Datt,
-    (K > NewDistance -> 
-        change_distance(G, V, NewDistance), 
-        change_previous(G, V, Natt), 
-        modify_key(G, NewDistance, K, V); true),
+    K > NewDistance,
+    change_distance(G, V, NewDistance),
+    change_previous(G, V, Natt),
+    modify_key(G, NewDistance, K, V),
+    process_neighbors(G, Natt, Rest), !.
+
+distance_calc(G, Natt, V, K, Rest):-
+    edge(G, Natt, V, W),
+    distance(G, Natt, Datt),
+    NewDistance is W + Datt,
+    K =< NewDistance,
     process_neighbors(G, Natt, Rest).
 
 shortest_path(G, Source, V, Path) :- 
@@ -154,8 +184,6 @@ build_path(G, Source, V, [(G, Prev, V, W) | Path1]) :-
 
 :- dynamic heap/2, heap_entry/4.
 
-heap(H, S) :- clause(heap(H, S), true).
-
 %Predicato che crea un nuovo heap
 new_heap(H) :- heap(H, _S), !.
 new_heap(H) :- assert(heap(H, 0)), !.
@@ -175,44 +203,86 @@ empty(H) :- heap(H, 0).
 not_empty(H) :- heap(H, S), S > 0.
 
 %Predicato che ritorna true se l'elemento che gli passi è la testa della heap
-head(H, K, V) :- 
+head(H, _, _) :-
     heap_size(H, S),
-    S = 0 -> fail;
+    S =:= 0,
+    !,
+    fail.
+
+head(H, K, V) :-
+    heap_size(H, S),
+    S > 0,
     heap_entry(H, 1, K, V).
 
 %Predicato che quando inserisci un elemento fa diventare l'heap un minheap (vedi algo)
 heapify_up(H, I) :-
-    I > 1,  % Continue if the index is not the root
-    ParentI is I div 2,  % Compute the index of the parent
-    heap_entry(H, I, K, V),  % Get the key and value of the current entry
-    heap_entry(H, ParentI, ParentK, ParentV),  % Get the key and value of the parent entry
-    (K < ParentK ; K =:= ParentK),  % Continue if the key of the current entry is less than the key of the parent entry, or if the keys are equal and the value of the current entry is less than the value of the parent entry
-    % Swap the current entry and the parent entry
+    I > 1,
+    ParentI is I div 2,
+    heap_entry(H, I, K, V),
+    heap_entry(H, ParentI, ParentK, ParentV),
+    K < ParentK,
     retractall(heap_entry(H, I, K, V)),
     retractall(heap_entry(H, ParentI, ParentK, ParentV)),
     assert(heap_entry(H, I, ParentK, ParentV)),
     assert(heap_entry(H, ParentI, K, V)),
-    heapify_up(H, ParentI).  % Recursively heapify up from the parent index
-heapify_up(_, _).  % Base case: do nothing
+    heapify_up(H, ParentI), !.
+
+heapify_up(H, I) :-
+    I > 1,
+    ParentI is I div 2,
+    heap_entry(H, I, K, V),
+    heap_entry(H, ParentI, ParentK, ParentV),
+    K =:= ParentK,
+    retractall(heap_entry(H, I, K, V)),
+    retractall(heap_entry(H, ParentI, ParentK, ParentV)),
+    assert(heap_entry(H, I, ParentK, ParentV)),
+    assert(heap_entry(H, ParentI, K, V)),
+    heapify_up(H, ParentI), !.
+
+heapify_up(_, _).
 
 %Predicato che quando rimuovi un elemento fa diventare l'heap un minheap (vedi algo)
 heapify_down(H, I) :-
-    heap(H, _),  % Get the size of the heap
-    LeftI is 2 * I,  % Compute the index of the left child
-    RightI is 2 * I + 1,  % Compute the index of the right child
-    heap_entry(H, I, K, V),  % Get the key and value of the current entry
-    % Find the child with the smallest key
-    (heap_entry(H, LeftI, LeftK, _), LeftK < K -> MinI = LeftI, MinK = LeftK; MinI = I, MinK = K),
-    (heap_entry(H, RightI, RightK, _), RightK < MinK -> MinI = RightI, MinK = RightK; true),
-    MinI \= I,  % Continue if the current entry does not have the smallest key
-    % Swap the current entry and the smallest child
+    heap(H, _),
+    LeftI is 2 * I,
+    RightI is 2 * I + 1,
+    heap_entry(H, I, K, V),
+    heap_entry(H, LeftI, LeftK, _),
+    LeftK < K,
+    MinI = LeftI, MinK = LeftK,
+    check_right_child(H, RightI, MinI, MinK, I, K, V), !.
+
+heapify_down(H, I) :-
+    heap(H, _),
+    LeftI is 2 * I,
+    RightI is 2 * I + 1,
+    heap_entry(H, I, K, V),
+    \+ (heap_entry(H, LeftI, LeftK, _), LeftK < K),
+    MinI = I, MinK = K,
+    check_right_child(H, RightI, MinI, MinK, I, K, V), !.
+
+heapify_down(_, _).
+
+check_right_child(H, RightI, MinI, MinK, I, K, V) :-
+    heap_entry(H, RightI, RightK, _),
+    RightK < MinK,
+    MinI = RightI, MinK = RightK,
+    swap_and_heapify(H, MinI, MinK, I, K, V), !.
+
+check_right_child(H, RightI, MinI, MinK, I, K, V) :-
+    \+ (heap_entry(H, RightI, RightK, _), RightK < MinK),
+    swap_and_heapify(H, MinI, MinK, I, K, V).
+
+swap_and_heapify(H, MinI, MinK, I, K, V) :-
+    MinI \= I,
     heap_entry(H, MinI, MinK, MinV),
     retractall(heap_entry(H, I, K, V)),
     retractall(heap_entry(H, MinI, MinK, MinV)),
     assert(heap_entry(H, I, MinK, MinV)),
     assert(heap_entry(H, MinI, K, V)),
-    heapify_down(H, MinI).  % Recursively heapify down from the smallest child
-heapify_down(_, _).  % Base case: do nothing
+    heapify_down(H, MinI), !.
+
+swap_and_heapify(_, _, _, _, _, _).
 
 %Predicato che inserisce un elemento nella heap
 insert(H, K, V) :- 
@@ -223,35 +293,40 @@ insert(H, K, V) :-
     assert(heap_entry(H, NewS, K, V)), 
     heapify_up(H, NewS).
 
-% Caso base: se la lista è vuota, non fare nulla
-insert_all(_H, []).
-
-% Caso ricorsivo: inserisci il primo elemento della lista nella heap,
-% poi chiama ricorsivamente insert_all sulla coda della lista
-insert_all(H, [(K, V)|T]) :-
-    insert(H, K, V),
-    insert_all(H, T).
-
 %Predicato che rimuove un elemento dalla heap
+extract(H, _, _) :- 
+    heap(H, S), 
+    S =:= 0, 
+    !, 
+    fail.
+
 extract(H, K, V) :-
     heap(H, S), 
     S > 0, 
     heap_entry(H, 1, K, V), 
     heap_entry(H, S, LastK, LastV), 
+    NewS is S - 1, 
     retractall(heap_entry(H, 1, K, V)), 
     retractall(heap_entry(H, S, LastK, LastV)), 
-    NewS is S - 1, 
     retractall(heap(H, S)), 
     assert(heap(H, NewS)), 
     assert(heap_entry(H, 1, LastK, LastV)), 
     heapify_down(H, 1).
 
 %Predicato che modfica la chiave di un elemento nella heap
-modify_key(H, NewKey, OldKey, V) :- 
+modify_key(H, NewKey, OldKey, V) :-
+    NewKey < OldKey,
     heap_entry(H, I, OldKey, V), 
     retractall(heap_entry(H, I, OldKey, V)), 
     assert(heap_entry(H, I, NewKey, V)),
-    (NewKey < OldKey -> heapify_up(H, I) ; heapify_down(H, I)).
+    heapify_up(H, I), !.
+
+modify_key(H, NewKey, OldKey, V) :-
+    NewKey >= OldKey,
+    heap_entry(H, I, OldKey, V), 
+    retractall(heap_entry(H, I, OldKey, V)), 
+    assert(heap_entry(H, I, NewKey, V)),
+    heapify_down(H, I).
 
 %Predicato che restituisce la lista degli elementi della heap
 list_heap(H) :- listing(heap(H, _)), listing(heap_entry(H, _, _, _)).
@@ -308,3 +383,13 @@ test_3(G) :-
     new_vertex(G, b),
     new_edge(G, b, a, 1),
     new_edge(G, a, b, 1).
+
+    test_heap(H) :-
+        new_heap(H),
+        insert(H, 12, a),
+        insert(H, 7, b),
+        insert(H, 50, c),
+        insert(H, 12, d),
+        insert(H, 4, e),
+        insert(H, 6, f),
+        list_heap(H).
