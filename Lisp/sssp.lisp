ee7cc353798a17c14073a85426eb067610c7a1a4
arancinosbarazzino <samueleferrillo16@gmail.com>
2024-02-16 13:03:23 +0100

    Finita manipolazione dei grafi

--- Lisp/sssp.lisp
+++ Lisp/sssp.lisp
@@ -12,23 +12,36 @@
 ; --------------------- Creazione e manipolazione grafi ---------------------- ;
 
 (defun is-graph (graph-id)
-    (gethash graph-id *graphs*)) ;; se esiste ritorna il grafo, altrimenti nil
+    (gethash graph-id *graphs*)) 
 
 (defun new-graph (graph-id)
     (or (gethash graph-id *graphs*)
         (setf (gethash graph-id *graphs*) graph-id)))
 
 (defun delete-graph (graph-id)
-    (clrhash graph-id *graphs*)
-    (clrhash (graph-vertices (graph-id)) *vertices*)
-    (clrhash (graph-edges (graph-id)) *edges*))
+    (remhash graph-id *graphs*)
+    (delete-vertices graph-id)
+    (delete-edges graph-id))
 
 (defun new-vertex (graph-id vertex-id)
+    (if (not (is-graph graph-id))
+        (error "Graph not found"))
     (setf (gethash (list 'vertex graph-id vertex-id) *vertices*) t)
     (list 'vertex graph-id vertex-id))
 
 (defun is-vertex (graph-id vertex-id)
-    (not (null (gethash (list 'vertex graph-id vertex-id) *vertices*)))) 
+    (and (is-graph graph-id)
+     (not (null (gethash (list 'vertex graph-id vertex-id) *vertices*))))) 
+
+(defun delete-vertices (graph-id)
+  (maphash 
+    (lambda (k v) 
+      (when (and 
+             (listp k) 
+             (eq (first k) 'vertex) 
+             (eq (second k) graph-id))
+        (remhash k *vertices*))) 
+    *vertices*))
 
 (defun graph-vertices (graph-id)
   (let ((vertex-rep-list '()))
@@ -38,7 +51,11 @@
              *vertices*)
     vertex-rep-list))
 
-(defun new-edge (graph-id vertex-id vertex-id2 &optional weight) 
+(defun new-edge (graph-id vertex-id vertex-id2 &optional weight)
+    (if (not (and
+              (is-vertex graph-id vertex-id) 
+              (is-vertex graph-id vertex-id2)))
+        (error "Vertex not found"))
     (if (and (not (null weight)) (numberp weight) (> weight 0))
         (setf 
          (gethash (list 'edge graph-id vertex-id vertex-id2) *edges*) 
@@ -47,6 +64,17 @@
          (gethash (list 'edge graph-id vertex-id vertex-id2) 
           *edges*) 
          1)))
+
+(defun delete-edges (graph-id)
+  (maphash 
+    (lambda (k v) 
+      (when (and 
+             (listp k) 
+             (eq (first k) 'edge) 
+             (eq (second k) graph-id))
+        (remhash k *edges*))) 
+    *edges*))
+
 
 (defun graph-edges (graph-id)
     (let ((vertex-rep-list '()))
@@ -70,18 +98,20 @@
                         (push (list k v) neighbors))) *edges*)neighbors))
 
 (defun graph-print (graph-id)
-    (format t "Vertices: ~a~%" (graph-vertices graph-id))
-    (format t "Edges: ~a~%" (graph-edges graph-id)))
-    ; --------------------- heap ---------------------- ;
+  (format t "Vertices: ~a~%" (graph-vertices graph-id))
+  (format t "Edges: ~a~%" (graph-edges graph-id))
+  (values))
+
+; -------------------------- Algoritmo di MinHeap ---------------------------- ;
     
-    
-    (defun new-heap (heap-id &optional (capacity 42))
-       (or (gethash heap-id *heaps*)
-       (setf (gethash heap-id *heaps*)
-       (list ’heap heap-id 0 (make-array capacity)))))
+
+(defun new-heap (heap-id &optional (capacity 42))
+   (or (gethash heap-id *heaps*))
+   (setf (gethash heap-id *heaps*))
+   (list ’heap heap-id 0 (make-array capacity)))
 
 
 (defun heap-delete(heap-id)
     (clrhash heap-id *heaps*))
 
-heap-empty    
+;; heap-empty    
