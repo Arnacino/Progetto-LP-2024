d184674bfc7aadab8355b355961e4f7d61f64671
arancinosbarazzino <samueleferrillo16@gmail.com>
2024-02-23 18:54:52 +0100

    aggiunti commenti da una riga

--- Prolog/sssp.pl
+++ Prolog/sssp.pl
@@ -6,33 +6,41 @@
 
 :- dynamic graph/1, vertex/2, edge/4, distance/3, visited/2, previous/3.
 
+% Predicato che ha successo se G è un grafo
 graph(G) :- clause(graph(G), true).
 
+% Predicato che ha successo se V è un vertice del grafo G
 vertex(G, V) :- clause(vertex(G, V), true).
 
+% Predicato che ha successo se (U, V, W) è un arco del grafo G
 edge(G, U, V, W) :- clause(edge(G, U, V, W), true).
 
+% Predicato che crea un nuovo grafo se non esiste già
 new_graph(G) :- graph(G), !.
 new_graph(G) :- assert(graph(G)), !.
 
+% Predicato che cancella un grafo e tutti i suoi vertici e archi
 delete_graph(G) :- 
     graph(G),
     retractall(graph(G)),
     retractall(vertex(G, _)),
     retractall(edge(G, _, _, _)).
 
+% Predicato che crea un nuovo vertice se non esiste già
 new_vertex(G, V) :- 
     graph(G),
     vertex(G, V), !.
-
 new_vertex(G, V) :- 
     retractall(vertex(G, V)), 
     assert(vertex(G, V)), !.
 
+% Predicato che ritorna true se Vs è una lista di tutti i vertici del grafo G
 vertices(G, Vs) :- findall(V, vertex(G, V), Vs).
 
+% Predicato che stampa una lista di tutti i vertici del grafo G
 list_vertices(G) :- listing(vertex(G, _)).
 
+% Predicato che crea un nuovo arco se non esiste già con peso di default 1
 new_edge(G, U, V) :- new_edge(G, U, V, 1).
 new_edge(G, U, V, _) :- edge(G, U, V, _), !.
 new_edge(G, U, V, Weight) :- 
@@ -40,15 +48,20 @@ new_edge(G, U, V, Weight) :-
     assert(edge(G, U, V, Weight)),
     !.
 
+% Predicato che ritorna true se Es è una lista di tutti gli archi del grafo G
 edges(G, Es) :- 
     findall((U, V, Weight), edge(G, U, V, Weight), Es).
 
+% Predicato che ritorna true se Ns è 
+% una lista di tutti i vertici adiacenti a V nel grafo G
 neighbors(G, V, Ns) :- 
     vertex(G, V), 
     findall((G, V, N, Weight), edge(G, V, N, Weight), Ns).
 
+% Predicato che stampa una lista di tutti gli archi del grafo G
 list_edges(G) :- listing(edge(G, _, _, _)).
 
+% Predicato che stampa il grafo G, compresi vertici e archi
 list_graph(G) :-
     writeln("Vertici:"),
     list_vertices(G),
@@ -59,17 +72,21 @@ list_graph(G) :-
 
 :- dynamic distance/3, previous/3, visited/2.
 
+% Predicato cambia la distanza di un vertice dalla sorgente
 change_distance(G, V, NewD) :- 
     retractall(distance(G, V, _)), 
     assert(distance(G, V, NewD)).
 
+% Predicato cambia il predecessore di un vertice
 change_previous(G, V, NewU) :- 
     retractall(previous(G, V, _)), 
     assert(previous(G, V, NewU)).
 
+% Predicato che asserisce un vertice come visitato
 visited(G, V) :- clause(visited(G, V), true), !.
 visited(G, V) :- assert(visited(G, V)).
 
+% Predicato che inizializza la distanza di tutti i vertici del grafo G
 initialize_distance(_, []) :- !.
 initialize_distance(G, [V | Rest]) :- 
     visited(G, V),
@@ -79,12 +96,14 @@ initialize_distance(G, [V | Rest]) :-
     assert(distance(G, V, inf)), 
     initialize_distance(G, Rest), !.
 
+% Predicato che inserisce tutti gli elementi del grafo G in una heap
 initialize_heap(_, []).
 initialize_heap(G, [V | Rest]) :- 
     distance(G, V, D),
     insert(G, D, V),
     initialize_heap(G, Rest), !.
 
+% Predicato che implementa l'algoritmo di Dijkstra
 dijkstra_sssp(G, Source) :-
     vertex(G, Source),
     retractall(distance(G, _, _)),
@@ -100,6 +119,7 @@ dijkstra_sssp(G, Source) :-
     process_neighbors(G, Source, Ns),
     dijkstra(G, Source), !.
 
+% Parte ricorsiva dell'algoritmo di Dijkstra
 dijkstra(G, Natt) :- 
     \+distance(G, Natt, 0),
     not_empty(G),
@@ -124,6 +144,8 @@ dijkstra(G, Natt) :-
     process_neighbors(G, Natt, Ns),!.
 dijkstra(G, _) :- empty(G).
 
+% Predicato che modifica la 
+% distanza di un vertice se è minore di quella attuale
 process_neighbors(_, _, []) :- !.
 process_neighbors(G, Np, [ (G, Np, Narr, W)| Rest]) :-
     \+visited(G, Narr),
@@ -136,7 +158,6 @@ process_neighbors(G, Np, [ (G, Np, Narr, W)| Rest]) :-
     change_previous(G, Narr, Np),
     modify_key(G, NewD, Da, Narr),
     process_neighbors(G, Np, Rest), !.
-
 process_neighbors(G, Np, [(G, Np, Narr, W)| Rest]) :-
     \+visited(G, Narr),
     vertex(G, Narr), 
@@ -149,12 +170,14 @@ process_neighbors(G, Np, [(G, Np, Narr, _)| Rest]) :-
     visited(G, Narr), 
     process_neighbors(G, Np, Rest), !.
 
+% Predicato che ritorna true se Path è il cammino più breve tra la Source e V
 shortest_path(G, Source, V, Path) :- 
     vertex(G, Source),
     vertex(G, V),
     build_path(G, Source, V, BuiltPath),
     reverse(BuiltPath, Path).
 
+%  Predicato che costruisce il cammino più breve tra la Source e V
 build_path(G, Source, V, [(G, Prev, V, W) | Path1]) :- 
     previous(G, V, Prev),
     edge(G, Prev, V, W),
@@ -166,19 +189,25 @@ build_path(_, Source, V, []) :-
 
 :- dynamic heap/2, heap_entry/4.
 
+% Predicato che crea una nuova heap con dimensione 0 se non esiste già
 new_heap(H) :- heap(H, _S), !.
 new_heap(H) :- assert(heap(H, 0)), !.
 
+% Predicato che cancella una heap e tutti i suoi elementi
 delete_heap(H) :- 
     retractall(heap(H, _S)),
     retractall(heap_entry(H, _, _, _)), !.
 
+% Predicato che ritorna true se H è una heap con dimensione S
 heap_size(H, S) :- heap(H, S).
 
+% Predicato che ritorna true se H è una heap vuota
 empty(H) :- heap(H, 0).
 
+% Predicato che ritorna true se H è una heap non vuota
 not_empty(H) :- heap(H, S), S > 0.
 
+% Predicato che ritorna true se (K, V) è il primo elemento della heap H
 head(H, _, _) :-
     heap_size(H, S),
     S =:= 0,
@@ -189,6 +218,8 @@ head(H, K, V) :-
     S > 0,
     heap_entry(H, 1, K, V).
 
+% Predicato che modifica la heap per ridare la proprietà di minheap
+% dopo l'inserimento di un nuovo elemento
 heapify_up(H, I) :-
     I > 1,
     ParentI is I div 2,
@@ -213,6 +244,8 @@ heapify_up(H, I) :-
     heapify_up(H, ParentI), !.
 heapify_up(_, _).
 
+% Predicato che modifica la heap per ridare la proprietà di minheap
+% dopo l'estrazione del primo elemento
 heapify_down(H, I) :-
     heap(H, _),
     LeftI is 2 * I,
@@ -267,6 +300,7 @@ heapify_down(H, I) :-
     assert(heap_entry(H, RightI, K, V)), !.
 heapify_down(_,_).
 
+% Predicato che inserisce un nuovo elemento nella heap
 insert(H, K, V) :- 
     heap(H, S), 
     NewS is S + 1, 
@@ -275,6 +309,7 @@ insert(H, K, V) :-
     assert(heap_entry(H, NewS, K, V)), 
     heapify_up(H, NewS).
 
+% Predicato che estrae il primo elemento dalla heap
 extract(H, _, _) :- 
     heap(H, S), 
     S =:= 0, 
@@ -300,6 +335,7 @@ extract(H, K, V) :-
     assert(heap_entry(H, 1, LastK, LastV)), 
     heapify_down(H, 1).
 
+% Predicato che modifica la chiave di un elemento della heap
 modify_key(H, NewKey, OldKey, V) :-
     NewKey < OldKey,
     heap_entry(H, I, OldKey, V), 
@@ -313,4 +349,5 @@ modify_key(H, NewKey, OldKey, V) :-
     assert(heap_entry(H, I, NewKey, V)),
     heapify_down(H, I).
 
+% Predicato che stampa una lista di tutti gli elementi della heap
 list_heap(H) :- listing(heap(H, _)), listing(heap_entry(H, _, _, _)).
